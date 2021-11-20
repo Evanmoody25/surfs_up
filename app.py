31bf39157405240a7aa59136457ca5f8ab55fe84
Evan Moody <evan.saikung@gmail.com>
2021-11-20 12:20:18 -0600

    odule9

---
+++ app.py
@@ -0,0 +1,61 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+import os 
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# Set Up the Database and Flask
+engine = create_engine("sqlite:///hawaii.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Create a New Flask App Instance
+#################################################
+app = Flask(__name__)
+
+# Create the Welcome Route
+# Module 9.5.2
+
+@app.route('/')
+def welcome():
+    return(
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+
+
+
